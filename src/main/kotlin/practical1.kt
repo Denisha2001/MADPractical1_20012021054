d40cc67dd5a9ea3393c97738bbbe293d1f52f970
Denisha2001 <savaliyadenisha143@gmail.com>
2022-08-05 09:02:42 +0530

    denisha

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,8 +1,6 @@
 fun main()
 {
-    println("Hello world")
-    var x:Int=1
-    val y:Int=1
+    println("Enter Number:")
+    var x= readLine()!!.toInt()
     println("x=$x")
-    println("y=$y")
 }
