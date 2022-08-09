966044eb2280ed1cad7dd24c4b3fc7a30d46f2e1
Denisha2001 <savaliyadenisha143@gmail.com>
2022-08-09 09:00:28 +0530

    pr4

--- src/main/kotlin/practical5.kt
+++ src/main/kotlin/practical5.kt
@@ -1,31 +1,31 @@
 fun main(){
-    fun Opration(op:Char,num1:Int):Int{
+    fun Opration(op:String,num1:Int):Int{
         when(op){
-            '1'->{
+            "1"->{
                 println("month is January")
             }
-            '2'->{
+            "2"->{
                 println("month is February")
             }
-            '3'->{
+            "3"->{
                 println("month is March")
             }
-            '4'->{
+            "4"->{
                 println("month is April")
             }
-            '5'->{
+            "5"->{
                 println("month is May")
             }
-            '6'->{
+            "6"->{
                 println("month is June")
             }
-            '7'->{
+            "7"->{
                 println("month is July")
             }
-            '8'->{
+            "8"->{
                 println("month is August")
             }
-            '9'->{
+            "9"->{
                 println("month is September")
             }
             "10"->{
