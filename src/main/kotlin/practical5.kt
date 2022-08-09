0f4a6f4ff039f5eca1196f86ddbf462714aa8a3f
Denisha2001 <savaliyadenisha143@gmail.com>
2022-08-09 08:53:58 +0530

    pr4

---
+++ src/main/kotlin/practical5.kt
@@ -0,0 +1,46 @@
+fun main(){
+    fun Opration(op:Char,num1:Int):Int{
+        when(op){
+            '1'->{
+                println("month is January")
+            }
+            '2'->{
+                println("month is February")
+            }
+            '3'->{
+                println("month is March")
+            }
+            '4'->{
+                println("month is April")
+            }
+            '5'->{
+                println("month is May")
+            }
+            '6'->{
+                println("month is June")
+            }
+            '7'->{
+                println("month is July")
+            }
+            '8'->{
+                println("month is August")
+            }
+            '9'->{
+                println("month is September")
+            }
+            "10"->{
+                println("month is october")
+            }
+            "11"->{
+                println("month is November")
+            }
+            "12"->{
+                println("month is December")
+            }
+        else ->{
+            println("Please Enter vaild number")
+        }
+        }
+
+    return  -1}
+}
---
+++ src/main/kotlin/practical6.kt
@@ -0,0 +1,24 @@
+fun main(){
+    fun opration(op:Char, vararg numarray:Int):Int {
+        when(op) {
+            '+' -> {
+                for (num in numarray)
+                    println("sum of num1 and num2 is")
+            }
+            '%' -> {
+                for (num in numarray)
+                    println("division of num1 and num2 is")
+            }
+            '*' -> {
+                for (num in numarray)
+                    println("multiplication of num1 and num2 is")
+            }
+            '-' -> {
+                for (num in numarray)
+                    println("subtraction of num1 and num2 is")
+            }
+
+        }
+    return -1}
+}
+
